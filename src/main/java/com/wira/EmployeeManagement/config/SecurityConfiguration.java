e891dd94a48bf8c056c51c977861f13dc4f18c92
mochwira <mochamadwirasanjaya@gmail.com>
2021-07-16 16:28:45 +0700

    Update layout and Fragment

--- src/main/java/com/wira/EmployeeManagement/config/SecurityConfiguration.java
+++ src/main/java/com/wira/EmployeeManagement/config/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
                 "/registration**",
-                "/js/**",
+                "/static/**",
                 "/css/**",
                 "/img/**").permitAll()
                 .anyRequest().authenticated()
