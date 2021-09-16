ccbfc00173550d2acc5b468d17907785bbbc1908
mochwira <mochamadwirasanjaya@gmail.com>
2021-09-16 16:57:31 +0700

    Fixing modal input in layout and add some configuration in controller

--- src/main/java/com/wira/EmployeeManagement/controller/ReportController.java
+++ src/main/java/com/wira/EmployeeManagement/controller/ReportController.java
@@ -1,9 +1,11 @@
 package com.wira.EmployeeManagement.controller;
 
 import com.wira.EmployeeManagement.model.Kategori;
+import com.wira.EmployeeManagement.model.Project;
 import com.wira.EmployeeManagement.model.Reports;
 
 import com.wira.EmployeeManagement.repository.KategoriRepository;
+import com.wira.EmployeeManagement.repository.ProjectRepository;
 import com.wira.EmployeeManagement.repository.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -25,6 +27,9 @@ public class ReportController {
     @Autowired
     public KategoriRepository kategoriRepository;
 
+    @Autowired
+    public ProjectRepository projectRepository;
+
 
     @GetMapping("/report")
     public String createIndex(Model model, @RequestParam(defaultValue = "0", name = "page") int page) {
@@ -33,25 +38,28 @@ public class ReportController {
         model.addAttribute("buatPageReport", page);
         model.addAttribute ( "buatReport", new Reports());
         model.addAttribute("kategoris", kategoriRepository.findAll());
+        model.addAttribute("projects", projectRepository.findAll());
         return "/report/report.html";
     }
 
     @PostMapping("/savereport")
-    public String save(@Valid Reports reports, Kategori kategori, BindingResult bindingResult) {
+    public String save(@Valid Reports reports, Kategori kategori, Project project, BindingResult bindingResult) {
         if(bindingResult.hasErrors()){
             return "redirect:/report";
         }
 
         reportRepository.save(reports);
         kategoriRepository.save(kategori);
+        projectRepository.save(project);
 
         return "redirect:/report";
     }
 
     @GetMapping("/deletereport")
-    public String deleteReport(@RequestParam("reportId") Long reportId, Integer kategoriId) {
+    public String deleteReport(@RequestParam("reportId") Long reportId, Integer kategoriId, Long projectId) {
         reportRepository.deleteById(reportId);
         kategoriRepository.deleteById(kategoriId);
+        projectRepository.deleteById(projectId);
 
         return "redirect:/report";
     }
